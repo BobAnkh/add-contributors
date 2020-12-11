ae5f2af391ef39c37a7118bb865aa6d1123f36df
Bailey Harrington <baileythegreen@gmail.com>
2020-12-11 21:54:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,7 @@ def generate_contributors(repo, COLUMN_PER_ROW, img_width, font_size,
             <img src={avatar_url} width="{img_width};"{img_style} alt={name}/>
             <br />
             <sub style="font-size:{font_size}px"><b>{name}</b></sub>
+            <sub></sub>
             <br />
         </a>
     </td>'''
